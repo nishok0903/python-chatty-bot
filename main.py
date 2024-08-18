b4a030c8255b80b32dd5adf2a8d93235e3a10188
nishok0903 <nishok0903@gmail.com>
2024-08-19 02:30:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     # This is the default and can be omitted
-    api_key=os.environ.get("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"),
+    api_key=os.environ.get(""),
 )
 
 chat_completion = client.chat.completions.create(
